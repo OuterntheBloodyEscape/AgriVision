4cca24f9704dfdfb18e9cdfd5aac17889489ffe8
MD. Rashidul Hasan <hamimhasan244@gmail.com>
2026-08-02 18:04:44 +0600

    welcome_screen_added

---
+++ src/login.jsx
@@ -0,0 +1,13 @@
+import './login.css'
+function login() {
+    return (
+        <>
+            <div id="bg_image"></div>
+            <div id='container'>
+                <h1 id='heading'>Login</h1>
+            </div>
+        </>
+    );
+}
+
+export default login
---
+++ src/welcome.jsx
@@ -0,0 +1,20 @@
+import info from './assets/info-recolored.png'
+import up_right from './assets/up-right-arrow.png'
+import './welcome.css'
+function welcome({ cl }) {
+    return (
+        <>
+            <div id="bg_image"></div>
+            <div id='container'>
+                <p id='heading'>Welcome to AgriVision</p>
+                <div id='button_section'>
+                    <div id='about'><p>About us</p><img src={info} height={20} width={20} draggable={false} /></div>
+                    <div id='join' onClick={cl}><p>Join us </p><img src={up_right} height={20} width={20} draggable={false} /></div>
+                </div>
+            </div>
+
+        </>
+    );
+}
+
+export default welcome
